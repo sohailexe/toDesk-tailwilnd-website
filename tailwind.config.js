71b37165bb6424e10980cfb694f12702e04c1356
Sohail Faiz Rasool <sa472811@gmail.com>
2024-11-19 22:28:00 +0500

    basic  nav with mobile first approach

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3238f2",
+        secondry: "#01ee91",
+      },
+      fontFamily: {
+        display: ["Poppins", "sans-serif"],
+
+        body: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+// Poppins
